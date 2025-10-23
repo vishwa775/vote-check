6aa1cf67696bb300056d09363b1b7cf754611b13
vishwa775 <vishwaamagi@gmail.com>
2025-10-23 23:17:09 +0530

    Create voting.py

---
+++ voting.py
@@ -0,0 +1,7 @@
+age = int(input ("Enter your age:")
+          if age<0:
+          print("age cant be negative")
+          elif age<=18:
+print("you are not eligible to vote")
+else:
+print("you are eligible to vote")
