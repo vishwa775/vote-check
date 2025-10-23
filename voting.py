cfc9853a54c1062f929e9d983c383c42cdfca847
vishwa775 <vishwaamagi@gmail.com>
2025-10-23 23:31:00 +0530

    Update voting.py

--- voting.py
+++ voting.py
@@ -1,7 +1,8 @@
-age = int(input ("Enter your age:"))
-          if age<0:
-                    print("age cant be negative")
-          elif age<=18:
-                    print("you are not eligible to vote")
-          else:
-                    print("you are eligible to vote")
+age = int(input("Enter your age: "))
+
+if age < 0:
+    print("Age can't be negative")
+elif age <= 18:
+    print("You are not eligible to vote")
+else:
+    print("You are eligible to vote")
